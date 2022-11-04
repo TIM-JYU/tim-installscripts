f18564c9e90cbfda64f1a6f443789e8b7c76f7de
Sami Viitanen <sami.a.viitanen@outlook.com>
2022-11-04 20:05:26 +0200

    Skip large downloads if file already exists; skip re-installing existing packages

--- arch.sh
+++ arch.sh
@@ -9,12 +9,16 @@ set -o pipefail
 sudo pacman -Syy
 # Required for non-Gnome desktop environments
 sudo pacman -S gnome-terminal
-sudo pacman -S ca-certificates curl gnupg python3 git
+sudo pacman -S --needed ca-certificates curl gnupg python3 git
 # Download and install Docker static binaries
-curl -LO https://download.docker.com/linux/static/stable/x86_64/docker-20.10.9.tgz
+if ![ -f ./docker-20.10.9.tgz]; then
+    curl -LO https://download.docker.com/linux/static/stable/x86_64/docker-20.10.9.tgz
+fi
 tar xzvf docker-20.10.9.tgz
 sudo cp ./docker/* /usr/bin
-curl -LO https://desktop.docker.com/linux/main/amd64/docker-desktop-4.13.1-x86_64.pkg.tar.zst
+if ![ -f ./docker-desktop-4.13.1-x86_64.pkg.tar.zst]; then
+    curl -LO https://desktop.docker.com/linux/main/amd64/docker-desktop-4.13.1-x86_64.pkg.tar.zst
+fi
 sudo pacman -U ./docker-desktop-4.13.1-x86_64.pkg.tar.zst
 systemctl --user enable docker-desktop
 systemctl --user start docker-desktop
@@ -23,11 +27,11 @@ sudo groupadd docker
 
 if [[ "$*" == *"--profile dev"* ]]; then
     # Download nodejs, npm and pip
-    sudo pacman -S nodejs npm python-pip python-virtualenv dbus-x11
+    sudo pacman -S --needed nodejs npm python-pip python-virtualenv dbus-x11
     # Downgrade npm to 6.x
     sudo npm install -g npm@6
     # install snap
-    sudo pacman -S snapd
+    sudo pacman -S --needed snapd
     # install pycharm
     sudo snap install pycharm-professional --classic
 fi
