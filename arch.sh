49ec58e6d54d7a09d97165328eddb43bf3663a21
Sami Viitanen <sami.a.viitanen@outlook.com>
2024-11-04 17:21:13 +0200

    linux: Arch-specific installation for snapd

--- arch.sh
+++ arch.sh
@@ -21,7 +21,19 @@ if [[ "$*" == *"--profile dev"* ]]; then
     # Downgrade npm to 6.x
     npm install -g npm@6
     # install snap
-    pacman -S --noconfirm --needed snapd
+    echo "Installing Snap environment"
+    # note: snapd isn't officially available for vanilla arch, so we install it from AUR
+    if [ "$DISTRO" == "arch" ]; then
+        cd ~/.tim-install
+        git clone https://aur.archlinux.org/snapd.git && cd ~/.tim-install/snapd
+        # install deps, build package and install it
+        # the locale setting LC_ALL=C needs to be passed to makepkg
+        LC_ALL=C makepkg -s --noconfirm --needed -i
+    else
+        # manjaro has pre-built snapd in the official repos
+        pacman -S --noconfirm --needed snapd
+    fi
+
     # don't create the symlink if it already exists, as the script will error out
     if [ ! -L "/snap" ]; then
         # symlink workaround for snap
